962e576d9b0e5ebb0ce789b7465a0e93e6ec0b72
Jaime Sanders <jaimelyn517@gmail.com>
2020-06-17 14:57:35 -0700

    writes code to copy a read file

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,16 @@
-// Read a file
 const fsPromises = require('fs').promises;
 
+// Read a file
 fsPromises.readFile('./README.md', { encoding: 'utf8' })
   .then(console.log);
 
 // Write a file
-fsPromises.writeFile('./new-test-file.txt', 'this is a new file with new text')
+fsPromises.writeFile('./test-file.txt', 'this is a new file with new text')
   .then(console.log('done!'));
 
 // Copy a file
+const promiseToReadFile = fsPromises.readFile('./test-file.txt', { encoding: 'utf8' });
+
+promiseToReadFile
+  .then(contents => fsPromises.writeFile('./new-test-file', contents)
+    .then(console.log('Done with both!')));
