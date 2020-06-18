e690e2fb1ba4e4ed5d8db5d2d2cc71c7f07ffea7
Jaime Sanders <jaimelyn517@gmail.com>
2020-06-17 17:50:02 -0700

    adds superagent file with api calls

---
+++ superagent.js
@@ -0,0 +1,33 @@
+const request = require('superagent');
+
+// use superagent to make a get request to http://futuramaapi.herokuapp.com/api/quotes/1
+// use then to wait for the request to resolve and print the output
+request.get('http://futuramaapi.herokuapp.com/api/quotes/1')
+  .then(({ body }) => ({
+    character: body[0].character,
+    quote: body[0].quote,
+    image: body[0].image
+  }))
+  .then(quote => console.log(quote));
+
+// How would you get more than 1 quote?
+request.get('http://futuramaapi.herokuapp.com/api/quotes/3')
+  .then(({ body }) => {
+    return body.map(character => ({
+      character: character.character,
+      quote: character.quote,
+      image: character.image
+    }));
+  })
+  .then(quote => console.log(quote));
+
+// How would you get only quotes by Bender?
+request.get('http://futuramaapi.herokuapp.com/api/characters/bender')
+  .then(({ body }) => {
+    return body.map(character => ({
+      character: character.character,
+      quote: character.quote,
+      image: character.image
+    }));
+  })
+  .then(quote => console.log(quote));
